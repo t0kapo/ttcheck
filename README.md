be0b9cdb00de2ad55274cb3e11cdc941faf3e351
tokap0 <takachin5353@gmail.com>
2021-11-14 04:11:19 +0900

    The action is ready

---
+++ README.md
@@ -0,0 +1,48 @@
+# ttcheck   
+
+This Action checks for "Trusted Types" and "policy" for Trusted Types in the HTML and transforms HTML code as necessary.  
+
+## Usage  
+
+In this example workflow, the action checks the `Trusted Types` and `policy` in  `template/index.html (relative path)`  and transform the code as necessary. The "js-policy-file-path(here is "policy.js")" is explained later.
+```yaml
+steps: 
+    - uses: actions/checkout@v2    
+    - name: Check policy in HTML    
+      id: tt    
+      uses: t0kapo/ttcheck@v1.1    
+      with:
+        html-file-path: template/index.html     
+        js-policy-file-path: policy.js    
+    - name: Output run time    
+      run: echo "This program took ${{ steps.tt.outputs.time }} seconds to run!"   
+    - uses: stefanzweifel/git-auto-commit-action@v4.12.0   
+```    
+## Inputs
+
+## `html-file-path`    
+"html-file-path" specifies a path to the file checking Trusted Types and policy are effective (seeing meta tag).     
+ex:  `index.html`,  `template/index.html`      
+    
+      
+1.If "Trusted Types" is not enabled   
+    Insert the meta tag that enables Trusted Types(ex.  <meta http-equiv="Content-Security-Policy" content="require-trusted-types-for 'script';  trusted-types *** ">).
+2.If "Trusted Types" is enabled   
+    Nothing to do.   
+   
+  
+## `js-policy-file-path`     
+"js-policy-file-path" specifies a path to the file that you want to insert policy for Trusted Types.    
+example: `policy.js`, `static/js/policy.js`    
+     
+     
+1.If the policy is not in place    
+    Insert the policy name to the meta tag (ex(policy name is "mypolivy"). <meta http-equiv="Content-Security-Policy" content="require-trusted-types for 'script';  trusted-types mypolicy">).  
+2.If the policy is already in place     
+    Nothing to do.
+
+## Outputs   
+
+## `time`    
+`time` gets run time of this program.    
+
---
+++ add_tag.js
@@ -0,0 +1,68 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const jquery = require("jquery");
+const fs = require("fs");
+const beautify_html = require("js-beautify").html;
+
+//GitHub Actions用のToolkit
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+//オーバーヘッド(処理時間)の計測用
+const { performance } = require('perf_hooks');
+
+try {
+	const starttime = performance.now(); //計測開始
+
+	//assign html file and policy javascript(japanese:HTMLファイルとJSファイルの中身を変数へ格納)
+	var file_html = String(core.getInput('html-file-path'));
+	var file_js = String(core.getInput('js-policy-file-path'));
+	let html = fs.readFileSync(file_html,"utf8");
+	let jscript = fs.readFileSync(file_js,"utf8");
+
+	//change html using dom operation(jQuery) (japanese: 取得したHTMLをjsdomでDOMツリーにして、jQueryでDOM操作を行えるようにする)
+	var dom = new JSDOM(html);
+	const $ = jquery(dom.window);
+
+	if ($('meta[http-equiv="Content-Security-Policy"]').length){    //Trusted Types有効化用のmetaタグ(CSPヘッダ用)がある場合
+		var check = String($('meta[http-equiv="Content-Security-Policy"]').attr("content"));
+
+		const words = jscript.split(' ');
+		const policy_name = words[1];
+		const str_check =  new RegExp(policy_name);
+
+		if (str_check.test(check) ==  true){		//既に指定したポリシーがある場合
+			console.log("The policy '"+ policy_name + "' already exists!!");									//何もしない 
+		}else{
+			console.log("add new policy!");
+
+			$('meta[http-equiv="Content-Security-Policy"]').attr("content", function(index, val){ //新しいポリシーの場合、新しいポリシー名とポリシーを追加する
+				return val + ' ' + policy_name;
+			});
+			$("body").prepend('<script id="trusted_types">' + jscript + '</script>');
+		}
+	}else{
+		console.log("Activate Trusted Types and add new policy!!");
+
+		const words = jscript.split(' ');
+		const policy_name = words[1];
+
+		$("head").append('<meta http-equiv="Content-Security-Policy" content="require-trusted-types-for \'script\';  trusted-types ' + policy_name +'">');
+		$("body").prepend('<script id="trusted_types">' + jscript + '</script>');
+	}
+	
+	var change_dom = dom.serialize();
+	//make code beautiful
+	var code_beautifier = beautify_html(change_dom);
+	//overwrite html file
+	fs.writeFile(file_html, code_beautifier, (err) => {
+		if (err) throw err;
+		console.log('');
+	});
+
+	const endtime = performance.now(); //計測終了
+	const run_time = endtime - starttime;
+	core.setOutput('time', run_time);
+} catch (error) {
+	core.setFailed(error.message);
+}
